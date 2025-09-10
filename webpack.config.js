eb7b78234b8bf15764ae5ffc9cb99329bc85ca43
Lucas Becker <runxel@users.noreply.github.com>
2025-09-10 22:21:23 +0200

    Add webpack configuration for project setup

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production';
+
+	return {
+		entry: './src/main.ts',
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+			clean: true,
+			// Set public path for GitHub Pages
+			publicPath: isProduction ? '/bcf-viewer/' : '/',
+		},
+		resolve: {
+			extensions: ['.ts', '.js'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/,
+					use: [
+						isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+						'css-loader'
+					],
+				},
+				{
+					test: /\.(png|jpg|gif|svg)$/,
+					type: 'asset/resource',
+				},
+			],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './src/index.html',
+				filename: 'index.html',
+				// Process favicon paths based on public path
+				templateParameters: {
+					publicPath: isProduction ? '/bcf-viewer/' : '/',
+				},
+			}),
+			new CopyWebpackPlugin({
+				patterns: [
+					{
+						from: 'public',
+						to: '.',
+						globOptions: {
+							ignore: ['**/.*'],
+						},
+					},
+				],
+			}),
+			...(isProduction ? [
+				new MiniCssExtractPlugin({
+					filename: 'styles/main.css',
+				})
+			] : []),
+		],
+		devtool: 'source-map',
+		devServer: {
+			static: './dist',
+			hot: true,
+		},
+	};
+};
